1a4d2b6a9d28892517ed1c3bb9ecb990b29a645b
Josh Darling <joshua.a.darling@gmail.com>
2022-12-27 11:13:41 -0700

    merge and mergeSort() all tests passed

--- merge.js
+++ merge.js
@@ -1,7 +1,39 @@
-function merge(arr) {
+function merge(arr1, arr2) {
+    let j = 0
+    let i = 0
+    let results = []
+    while(j < arr1.length && i < arr2.length){
+        if(arr1[j] <= arr2[i]){
+            results.push(arr1[j])
+            j++
+
+        }else{
+            results.push(arr2[i])
+            i++
+        }
+
+    }
+
+    while(j < arr1.length){
+        results.push(arr1[j])
+        j++
+    }
+    while(i < arr2.length){
+        results.push(arr2[i])
+        i++
+    }
+
+    return results
 
 }
 
-function mergeSort() {}
+function mergeSort(arr) {
+    if(arr.length <= 1) return arr
+    let mid = Math.floor(arr.length/2)
+    let left = mergeSort(arr.slice(0,mid))
+    let right = mergeSort(arr.slice(mid))
+    
+    return merge(left, right)
+}
 
 module.exports = { merge, mergeSort};
