da0b105becc935bc80b95f928aa5367248497d67
Josh Darling <joshua.a.darling@gmail.com>
2022-12-28 11:25:53 -0700

    selection() tests passed

--- insertion.js
+++ insertion.js
@@ -1,3 +1,14 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++){
+        let curr = arr[i]
+        for(let j = 0; j > i; j++){
+            if(curr < arr[j]){
+                
+            }
+
+        }
+    }
+
+}
 
 module.exports = insertionSort;
--- selection.js
+++ selection.js
@@ -1,3 +1,43 @@
-function selectionSort() {}
+/* function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let curr = arr[i];
+    let smallest = curr;
+    let idx = 0;
+    for (let j = i; j <= arr.length; j++) {
+      if (arr[j] < smallest) {
+        smallest = arr[j];
+        idx = j;
+        
+      }
+    }
+    if(smallest !== curr){
+    arr[i] = smallest;
+    arr[idx] = curr;
+
+    }
+    
+  }
+  return arr;
+  
+} */
+
+function selectionSort(arr) {
+    const swap = (arr, idx1, idx2) =>
+      ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+  
+    for (let i = 0; i < arr.length; i++) {
+      let lowest = i;
+  
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[lowest] > arr[j]) {
+          lowest = j;
+        }
+      }
+  
+      if (i !== lowest) swap(arr, i, lowest);
+    }
+  
+    return arr;
+  }
 
 module.exports = selectionSort;
