ced80e9533f3ff3a91ee67c91b487a1f956dc977
Josh Darling <joshua.a.darling@gmail.com>
2022-12-28 11:31:02 -0700

    all tests passed

--- insertion.js
+++ insertion.js
@@ -1,14 +1,15 @@
 function insertionSort(arr) {
-    for(let i = 1; i < arr.length; i++){
-        let curr = arr[i]
-        for(let j = 0; j > i; j++){
-            if(curr < arr[j]){
-                
-            }
-
-        }
+    for (let i = 0; i < arr.length; i++) {
+      let currentValue = arr[i];
+  
+      for (var j = i - 1; j > -1 && arr[j] > currentValue; j--) {
+        arr[j + 1] = arr[j];
+      }
+  
+      arr[j + 1] = currentValue;
     }
-
-}
+  
+    return arr;
+  }
 
 module.exports = insertionSort;
